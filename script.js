f9579163ef13da81e80a067ce96f830576c67c60
jessicalima22 <jessica.lima22@hotmail.com>
2023-10-09 23:56:06 -0300

    exploring fetch

--- script.js
+++ script.js
@@ -102,3 +102,13 @@ getCountryAndNeighbour('brasil');
 
 const request = fetch(`https://restcountries.com/v2/name/portugal`);
 console.log(request);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`).then(function (
+    response
+  ) {
+    console.log(response);
+  });
+};
+
+getCountryData('usa');
